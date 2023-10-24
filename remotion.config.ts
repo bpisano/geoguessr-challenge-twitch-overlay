bfce27b6fa5275ef8cefbbc3419eebda9d9d4c08
Benjamin Pisano <benjamin.pisano@icloud.com>
2023-10-24 10:02:04 +0200

    updated dependencies

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,4 @@
-import { Config } from 'remotion';
+import { Config } from '@remotion/cli/config';
 import { webpackOverride } from './src/utils/webpackOverride';
 
 Config.setOverwriteOutput(true);
